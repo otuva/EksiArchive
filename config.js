e257d309f88027edd6799bac88ac5eb24d2dd31d
Onur Alp Akin <67955086+otuva@users.noreply.github.com>
2021-10-17 08:56:40 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,11 @@
+// databaseFileName = "database"
+databaseFilePath = "/home/tfp/Programming/node/EksiArchive/database.db"
+// get abs path from relative path
+
+// random user agent on
+requestHeaders = {
+    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+}
+
+module.exports.databaseFilePath = databaseFilePath;
+module.exports.requestHeaders = requestHeaders;
