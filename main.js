aae0f5516553df349c9baa8f6e973f922019a2ad
Onur Alp Akin <67955086+otuva@users.noreply.github.com>
2021-10-21 12:43:05 +0300

    improve existing flags & moved functionality of page flag to user flag

--- main.js
+++ main.js
@@ -38,14 +38,11 @@ else {
     else {
         try {
             // console.log(argv);
-            if (typeof argv.sleep === 'number') {
-                config.entry.sleep = argv.sleep;
-            }
-            if (typeof argv.force === 'boolean') {
-                config.entry.force = argv.force;
-            }
+            if (typeof argv.sleep === 'number') config.entry.sleep = argv.sleep;
+            if (typeof argv.force === 'boolean') config.entry.force = argv.force;
 
-            if (argv.e) {
+            if ((argv.e || argv.entry) && !(argv.e && argv.entry)) {
+                if (argv.entry) argv.e = argv.entry;
                 if (typeof argv.e === 'string' || typeof argv.e === 'number') {
                     const id = inputValidate.isInputEntryLink(argv.e.toString());
                     // console.log(id);
@@ -57,40 +54,79 @@ else {
                     console.error('entry kismi bos olamaz.');
                 }
             }
-            else if (argv.p) {
-                if (typeof argv.p === 'string') {
-                    if (inputValidate.isPageArgumentValid(argv.p)) {
-                        console.log(`arsivlenecek sayfa: ${argv.p}`);
-                        const values = argv.p.split(",");
-                        const user = values[0].replace(/ /g, '-');
-                        userOps.archiveEntriesInAPage(`/basliklar/istatistik/${user}/son-entryleri?p=${values[1]}`);
-                    }
-                    else {
-                        console.error('kullanici ya da sayfa gecerli degil.' + 'insert hata here');
-                    }
-                }
-                else {
-                    console.error('sayfa kismi bos olamaz');
-                }
+            // else if (argv.p) {
+            //     if (typeof argv.p === 'string') {
+            //         if (inputValidate.isPageArgumentValid(argv.p)) {
+            //
+            //             const values = argv.p.split(",");
+            //             const user = values[0].replace(/ /g, '-');
+            //             userOps.archiveEntriesInAPage(`/basliklar/istatistik/${user}/son-entryleri?p=${values[1]}`);
+            //         }
+            //         else {
+            //             console.error('kullanici ya da sayfa gecerli degil.' + 'insert hata here');
+            //         }
+            //     }
+            //     else {
+            //         console.error('sayfa kismi bos olamaz');
+            //     }
+            //
+            // }
+            else if ((argv.u || argv.user) && !(argv.u && argv.user)) {
+                if (argv.user) argv.u = argv.user;
 
-            }
-            else if (argv.u) {
                 if (typeof argv.u === 'string') {
-                    if (inputValidate.isUserValid(argv.u)) {
-                        const user = argv.u.replace(/ /g, '-');
-                        console.log(`arsivlenecek kullanici: ${user}`)
-                        userOps.archiveUser(user);
+                    if (inputValidate.isUserPageValid(argv.u)) {
+
+                        const values = argv.u.split(",");
+                        const user = values[0].replace(/ /g, '-');
+
+                        if (values.length === 1) {
+                            console.log(`arsivlenecek kullanici: ${user}`)
+                            userOps.archiveConsecutivePages(`/basliklar/istatistik/${user}/son-entryleri`);
+                        }
+                        else {
+                            console.log(`kullanici: ${user} - arsivlenecek sayfa: ${values[1]}`);
+                            userOps.archiveEntriesInAPage(`/basliklar/istatistik/${user}/son-entryleri?p=${values[1]}`);
+                        }
+
                     }
                     else {
-                        console.error('kullanici gecerli degil')
+                        console.error('kullanici veya sayfa gecerli degil')
                     }
                 }
                 else {
                     console.error('kullanici bos olamaz');
                 }
             }
+            // else if ((argv.f || argv.favorite) && !(argv.f && argv.favorite)) {
+            //     if (argv.favorite) argv.f = argv.favorite;
+            //
+            //     if (typeof argv.f === 'string') {
+            //         if (inputValidate.isUserPageValid(argv.f)) {
+            //
+            //             const values = argv.f.split(",");
+            //             const user = values[0].replace(/ /g, '-');
+            //
+            //             if (values.length === 1) {
+            //                 console.log(`favorileri arsivlenecek kullanici: ${user}`)
+            //                 userOps.archiveConsecutivePages(`/basliklar/istatistik/${user}/favori-entryleri`);
+            //             }
+            //             else {
+            //                 console.log(`kullanici: ${user} - arsivlenecek favori sayfa: ${values[1]}`);
+            //                 userOps.archiveEntriesInAPage(`/basliklar/istatistik/${user}/favori-entryleri?p=${values[1]}`);
+            //             }
+            //
+            //         }
+            //         else {
+            //             console.error('kullanici veya sayfa gecerli degil')
+            //         }
+            //     }
+            //     else {
+            //         console.log('insert favori hatasi here');
+            //     }
+            // }
             else {
-                console.log(`bilinmeyen flag. dogru kullanim:`+arguments);
+                console.log(`bilinmeyen veya hatali flag kullanimi. dogru kullanim:`+arguments);
             }
         }
         catch (e) {
