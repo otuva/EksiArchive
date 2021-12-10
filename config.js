d10f4937e729c45a2a8748a2f76ce491362433de
Onur Alp Akin <67955086+otuva@users.noreply.github.com>
2021-12-10 11:53:10 +0300

    change dbfilepath to relative path

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// databaseFileName = "database"
-databaseFilePath = "/home/tfp/Programming/node/EksiArchive/database.db"
-// get abs path from relative path
+const path = require('path');
 
-// random user agent on
+databaseFilePath = path.join(process.cwd(), 'database.db');
+
+// add option for random user agent on
 requestHeaders = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
